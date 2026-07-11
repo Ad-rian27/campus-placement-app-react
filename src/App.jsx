ecda70e4f8b34e9ecc7b38bb69e25e8cdf5efd47
Ad-rian27 <rian270627@gmail.com>
2026-07-11 12:10:40 +0530

    Added DeleteReg component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import AddReg from './components/AddReg'
 import SearchReg from './components/SearchReg'
 import ViewReg from './components/ViewReg'
+import DeleteReg from './components/DeleteReg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
       <AddReg />
       <SearchReg/>
       <ViewReg />
+      <DeleteReg />
     </>
   )
 }
